d322e607f31ca924d17ec1062b1e3a5b0ef46480
Angel-enc <enock8876@gmail.com>
2025-07-15 09:04:45 +0000

    no consigo ocultar el icono

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import '@fortawesome/fontawesome-free/css/all.min.css';
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [inputValue, setInputValue] = useState (""); //Estado para lo que escribe el usuario.
+	const [tareas, setTareas] = useState ([]); //Estado para lista de tareas.
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	//Función para añadir tarea cuando se presiona "ENTER".
+	const pulsarTecla = (e) => {
+		if(e.key ==="Enter" && inputValue !== ""){
+			//Agrega tarea y limpia el input.
+			setTareas([...tareas, inputValue]);
+			setInputValue("");
+		}
+	};
+	//Función para eliminar tarea.
+	const eliminarTarea = (index) => {
+		const nuevasTareas = tareas.filter((_,i) => i !== index);
+		setTareas(nuevasTareas);
+	}
+	return (
+		<div className="container">
+			<h1>Lista de Tareas</h1>
+			<ul>
+				<li>
+					<input 
+					type="text" 
+					onChange={(e) => setInputValue(e.target.value)}
+					value ={inputValue}
+					onKeyUp={pulsarTecla}
+					placeholder="No hay tareas. Añadir tarea."></input>
+				</li>
+			
+			{tareas.map((tarea, index) => (
+				<li key={index}>
+					{tarea}
+					<i className="fa-solid fa-trash"
+					onClick={() => eliminarTarea(index)}></i>
+				</li>
+			
+			))}
+			</ul>
+			<div className="task-counter">{tareas.length}Item{tareas.length !== 1 ? "s" : ""} left
+
+			</div>
 		</div>
 	);
 };
