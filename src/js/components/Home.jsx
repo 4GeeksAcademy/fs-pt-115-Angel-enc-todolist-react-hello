bd31f737f8f47c67cab5d9c428e7d267570d5547
Angel-enc <enock8876@gmail.com>
2025-07-15 09:23:48 +0000

    no logro la manera de ocultar la papelera

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,14 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState (""); //Estado para lo que escribe el usuario.
-	const [tareas, setTareas] = useState ([]); //Estado para lista de tareas.
+	const [inputValue, setInputValue] = useState(""); //Estado para lo que escribe el usuario.
+	const [tareas, setTareas] = useState([]); //Estado para lista de tareas.
 
 	//Función para añadir tarea cuando se presiona "ENTER".
 	const pulsarTecla = (e) => {
-		if(e.key ==="Enter" && inputValue !== ""){
+		if (e.key === "Enter" && inputValue !== "") {
 			//Agrega tarea y limpia el input.
 			setTareas([...tareas, inputValue]);
 			setInputValue("");
@@ -16,7 +16,7 @@ const Home = () => {
 	};
 	//Función para eliminar tarea.
 	const eliminarTarea = (index) => {
-		const nuevasTareas = tareas.filter((_,i) => i !== index);
+		const nuevasTareas = tareas.filter((tarea, i) => i !== index);
 		setTareas(nuevasTareas);
 	}
 	return (
@@ -24,22 +24,22 @@ const Home = () => {
 			<h1>Lista de Tareas</h1>
 			<ul>
 				<li>
-					<input 
-					type="text" 
-					onChange={(e) => setInputValue(e.target.value)}
-					value ={inputValue}
-					onKeyUp={pulsarTecla}
-					placeholder="No hay tareas. Añadir tarea."></input>
+					<input
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyUp={pulsarTecla}
+						placeholder="No hay tareas. Añadir tarea."></input>
 				</li>
-			
-			{tareas.map((tarea, index) => (
-				<li key={index}>
-					{tarea}
-					<i className="fa-solid fa-trash"
-					onClick={() => eliminarTarea(index)}></i>
-				</li>
-			
-			))}
+
+				{tareas.map((tarea, index) => (
+					<li key={index}>
+						{tarea}
+						<i className="fa-solid fa-trash"
+							onClick={() => eliminarTarea(index)}></i>
+					</li>
+
+				))}
 			</ul>
 			<div className="task-counter">{tareas.length}Item{tareas.length !== 1 ? "s" : ""} left
 
